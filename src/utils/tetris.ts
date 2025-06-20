13821848cf40d60a5365f768e50e33a27b7f4012
zareen1729 <zareen1729@gmail.com>
2025-06-20 04:27:19 -0700

    Add files via upload

---
+++ src/utils/tetris.ts
@@ -0,0 +1,44 @@
+export const BOARD_WIDTH = 10;
+export const BOARD_HEIGHT = 20;
+
+export const TETROMINOS = {
+  I: [
+    [0, 0, 0, 0],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+  ],
+  O: [
+    [1, 1],
+    [1, 1]
+  ],
+  T: [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 0, 0]
+  ],
+  S: [
+    [0, 1, 1],
+    [1, 1, 0],
+    [0, 0, 0]
+  ],
+  Z: [
+    [1, 1, 0],
+    [0, 1, 1],
+    [0, 0, 0]
+  ],
+  J: [
+    [1, 0, 0],
+    [1, 1, 1],
+    [0, 0, 0]
+  ],
+  L: [
+    [0, 0, 1],
+    [1, 1, 1],
+    [0, 0, 0]
+  ]
+};
+
+export function createEmptyBoard(): number[][] {
+  return Array(BOARD_HEIGHT).fill(null).map(() => Array(BOARD_WIDTH).fill(0));
+}
